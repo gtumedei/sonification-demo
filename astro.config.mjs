55a67e3ce99fb5881864ee37052d8c7af72eab9b
gtumedei <giannitumedei@gmail.com>
2023-04-18 11:08:30 +0200

    Setup layout for all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+import icons from "unplugin-icons/vite"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind()
-  ]
+  ],
+  vite: {
+    plugins: [
+      icons({ compiler: "astro" })
+    ]
+  }
 })
