2378fb264696bd4525739b9d15971a0a3bb1aa18
gtumedei <giannitumedei@gmail.com>
2023-04-19 14:55:12 +0200

    Swap sample audio video for real assets

---
+++ src/cities.ts
@@ -0,0 +1,7 @@
+export const cities = [
+  { code: "cesena", name: "Cesena" },
+  { code: "londra", name: "Londra" },
+  { code: "nuovadelhi", name: "Nuova Delhi" },
+  { code: "pechino", name: "Pechino" },
+  { code: "roma", name: "Roma" }
+]
